34d3e774ba05463a84ab712bd0bbbe463954881d
Abdul-Azeez <zeezu124@gmail.com>
2023-11-03 00:24:37 +0000

    WIP: Added User seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'username' => 'devzeez',
+            'email' => 'devzeez@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
